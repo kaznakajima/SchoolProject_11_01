7102ab1bfe9286e813dda3dad056503e59021223
yuya0915 <nkc.yuya.mase.2016@gmail.com>
2017-11-10 14:36:28 +0900

    ろぐうううううううううううううううううううううううううううううううううううううう

---
+++ Assets/test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [AddComponentMenu("mase/test")]
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            Debug.LogFormat("Time:{0}", System.DateTime.Now);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
